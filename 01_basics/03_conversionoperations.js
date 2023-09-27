e037dbd1fcabb0a7fea85986a11d0ef36b1f2e44
keerthi vardhan <123969081+keerthikumanduri@users.noreply.github.com>
2023-09-27 05:39:09 +0000

    conversion of datatypes.

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,38 @@
+let score = "55abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//NOTES
+
+//"55" => 55 - whenever we try to convert a number to a number it can easily be converted to a number.
+
+//"55abc" => NaN - Whenever we try to convert a term like 55abc to a number it gives an output as NaN(not a number). but the type of 55abc is still a number.
+
+//true => 1; false =>0
+
+let isLoggedin = "" //here we are trying to check if the number 1 is boolean(true) or not.
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+//it is indeed boolean since the output we got is 1 which tells that it is TRUE.Therefore numbers can be converted to boolean.
+
+// when we assign a value of :
+
+// 1 => True; 0 => false
+// "" => False
+// "Rama" => True
+ 
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//here above we have converted a number to string.
