e85bb3d7bcfc5a0db9c935694502267e1d303c09
keerthikumanduri <keerthivardhan2k@gmail.com>
2023-09-29 14:48:16 +0530

    stack and heap memory.

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -33,3 +33,31 @@
  console.log(typeof (outsideTemp));// typeof tells us what datatype is used.
 
  //usually the type of all non primitive datatypes is always an object, but especially when the datatype of the fuction is checked it is called function object. this is not shown in the terminal but in the documentation.
+
+
+ //***********************************************************************
+
+ // stack memory (primitive), heap memory (non-primitive ).
+
+ // stack memory has all the primitive datatypes, for example when we do a console.log on primiive data types they are assigned a copy not the original form.
+
+ // heap is assigned a reference 
+
+ let infoOne = { 
+    name: "hanuman",
+    age: "~",
+ }
+
+ let infoTwo = { 
+    name: "hanuman",
+    age: "~",
+ }
+
+infoOne = infoTwo
+
+infoTwo.age = "Chiranjeevi"
+
+console.log(infoOne);
+console.log(infoTwo);
+
+// imagine stack cells one oveer the other. and imagine another big cell called heap. when different variable area created they are assigned some area in memory. all the primitive data types are only accessed from the same stack as an copy of the main file. but in the case of heap every variable is assigned an actual main reference . therefore in lines 46 to 61 two objects are defined and they are set equal, both of them actually access the main reference from the memory , hence if one of the value is changed the other changes too.
